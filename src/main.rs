9c87e91310d0fe451480577f7566d552c7fc6de1
KIMSeHyung <tpgud33@gmail.com>
2025-08-11 23:28:06 +0900

    function basic

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,20 @@
 
 use std::{mem::size_of};
 
-fn give_number() -> i32 {
+fn give_number(one: i32, two: i32) -> i32 {
     // return 37 or
-    37
+    one * two
 }
 
 fn empty_tuple() -> () {
+}
+
+fn mul_number(a: i32, b: i32) -> i32 {
+    let mul_by_ten = {
+        let first_number = 10;
+        first_number * a * b
+    };
+    mul_by_ten
 }
 
 fn main() {
@@ -23,7 +31,7 @@ fn main() {
     println!("size of string 'abc': {}", "가나다".len()); // byte 수
     println!("length of string 'abc': {}", "abc".chars().count());
 
-    println!("my age is {}", give_number());
+    println!("my age is {}", give_number(1, 2));
 
     println!("number {n}", n = my_number);
     println!("{0}, {1}, {0}", my_number, second_number);
@@ -32,5 +40,9 @@ fn main() {
 
     // Debug print
     println!("empty tuple: {:?}", _empty_tuple);
+
+    println!("mul {}", give_number(1, 2));
+
+    println!("mul by ten: {}", mul_number(10, 20));
 
 }
