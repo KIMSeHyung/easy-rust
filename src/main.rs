29f32d357b984df503fdf1f79fd96d5f93e4380a
KIMSeHyung <tpgud33@gmail.com>
2025-08-11 22:49:02 +0900

    sizeof char

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::mem::size_of;
 fn main() {
     println!("Hello, world!");
+    let my_number: i8 = 1;
+    let second_number: i16 = 10;
+
+    let third_number = my_number + second_number as i8; // type casting
+    println!("{}", third_number);
+
+    println!("size of char: {}", size_of::<char>());
 }
